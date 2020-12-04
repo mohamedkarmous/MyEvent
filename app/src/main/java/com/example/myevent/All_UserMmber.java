b1ad149620be9a1cff1d75c85784d2fcace2470e
mohamedkarmous <mohamedlahbibkarmous@gmail.com>
2020-12-05 00:19:26 +0100

    first commit

---
+++ app/src/main/java/com/example/myevent/All_UserMmber.java
@@ -0,0 +1,55 @@
+package com.example.myevent;
+
+public class All_UserMmber {
+    public All_UserMmber() {
+
+    }
+
+    String name;
+    String bio;
+    String prof;
+    String uid;
+    String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProf() {
+        return prof;
+    }
+
+    public void setProf(String prof) {
+        this.prof = prof;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+}
